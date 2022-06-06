2d4783638b2ca201f10a967880d385f681a05f9c
Hu's GitHub <alicehu0131@gmail.com>
2022-06-06 16:41:00 +0800

    Update test_functions.py

---
+++ tests/test_functions.py
@@ -0,0 +1,64 @@
+class AbstractionViolation(Exception):
+	pass
+
+def datatype(obj):
+	return type(obj).__name__
+
+
+class Abstract(object):
+	def __add__(self, other):
+		raise AbstractionViolation('can not add {} object to {}'.format(datatype(self), datatype(other)))
+
+
+	def __radd__(self, other):
+		raise AbstractionViolation('can not add {} object to {}'.format(datatype(self), datatype(other)))
+
+
+	def __eq__(self, other):
+		if isinstance(other, type(self)):
+			return other is not self
+
+		raise AbstractionViolation('can not use != on {} object and {}'.format(datatype(self), datatype(other)))
+
+
+
+	def __bool__(self):
+		raise AbstractionViolation('can not use {} object as a boolean'.format(datatype(self)))
+
+
+
+	def __getitem__(self, index):
+		raise AbstractionViolation('can not use [] notation on {} object'.format(datatype(self)))
+
+
+
+	def __iter__(self):
+		raise AbstractionViolation('can not iterate on {} object'.format(datatype(self)))
+
+
+
+	def __len__(self):
+		raise AbstractionViolation('can not use len notation on {} object'.format(datatype(self)))
+
+
+	def __call__(self, *args, **kwargs):
+		raise AbstractionViolation('can not call {} object'.format(datatype(self)))
+
+
+
+	def __hash__(self):
+		return id(self)
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
